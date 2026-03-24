d8b5e9b2927b1a4394ccac01dfe8b6e3ef60aaf5
David Baker Effendi <david@brokk.ai>
2026-03-24 15:41:08 +0200

    More robust null fix

--- tree-sitter-demo/src/main/java/org/treesitter/demo/Main.java
+++ tree-sitter-demo/src/main/java/org/treesitter/demo/Main.java
@@ -1,5 +1,6 @@
 package org.treesitter.demo;
 
+import org.jspecify.annotations.Nullable;
 import org.treesitter.TSNode;
 import org.treesitter.TSParser;
 import org.treesitter.TSTree;
@@ -12,23 +13,30 @@ public class Main {
             tsParser.setLanguage(new TreeSitterJson());
             TSTree tree = tsParser.parseString(null, "[1, null]");
             if (tree != null) {
-                TSNode rootNode = tree.getRootNode();
-                assert rootNode != null;
-                TSNode arrayNode = rootNode.getNamedChild(0);
-                assert arrayNode != null;
-                TSNode numberNode = arrayNode.getNamedChild(0);
-                assert rootNode.getType().equals("document");
-                assert arrayNode.getType().equals("array");
-                assert numberNode.getType().equals("number");
-                assert rootNode.getChildCount() == 1;
-                assert arrayNode.getChildCount() == 5;
-                assert arrayNode.getNamedChildCount() == 2;
-                assert numberNode.getChildCount() == 0;
-                String string = rootNode.toString();
+                String string = getString(tree);
                 System.out.printf("Syntax tree: %s\n%n", string);
             } else {
                 System.err.println("Syntax tree is null!");
             }
         }
+    }
+
+    private static @Nullable String getString(TSTree tree) {
+        TSNode rootNode = tree.getRootNode();
+        if (rootNode == null) return null;
+        TSNode arrayNode = rootNode.getNamedChild(0);
+        if (arrayNode == null) return null;
+        TSNode numberNode = arrayNode.getNamedChild(0);
+        if (numberNode == null) return null;
+
+        assert rootNode.getType().equals("document");
+        assert arrayNode.getType().equals("array");
+        assert numberNode.getType().equals("number");
+        assert rootNode.getChildCount() == 1;
+        assert arrayNode.getChildCount() == 5;
+        assert arrayNode.getNamedChildCount() == 2;
+        assert numberNode.getChildCount() == 0;
+
+        return rootNode.toString();
     }
 }
